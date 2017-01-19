cfecaec7d3fdb00b5c25775200dec5bd4a963975
mnh <martin.hylleberg@systematic.com>
2017-01-19 12:12:39 +0100

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import {NgModule, ErrorHandler} from '@angular/core';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {AboutPage} from '../pages/about/about';
+import {AddWorkoutPage} from '../pages/add-workout/add-workout';
+import {TabsPage} from '../pages/tabs/tabs';
+import {WorkoutsPage} from '../pages/workouts/workouts'
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    AddWorkoutPage,
+    WorkoutsPage,
+    TabsPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    AddWorkoutPage,
+    WorkoutsPage,
+    TabsPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {
+}
